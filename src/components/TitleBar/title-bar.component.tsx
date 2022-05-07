ff7878dd52219900f5845fe4e92982a5737664ad
Diego Ferreira <diegosf1705@gmail.com>
2022-05-07 20:57:03 -0300

    Improve TitleBar component layout

--- src/components/TitleBar/title-bar.component.tsx
+++ src/components/TitleBar/title-bar.component.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Pressable } from 'react-native';
 import uuid from 'react-native-uuid';
 import { useTheme } from 'styled-components';
 import { Feather } from '@expo/vector-icons';
