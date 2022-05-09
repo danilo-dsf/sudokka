06cc07943b72a7eff1cee066fb356e247a62053f
Danilo Ferreira <danilo1705@gmail.com>
2022-05-09 20:46:01 -0300

    Setup AdMob and add ads to the bottom of the app, when resuming a game and when wining

--- App.tsx
+++ App.tsx
@@ -3,6 +3,7 @@ import { StatusBar, StatusBarStyle } from 'expo-status-bar';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { ThemeProvider } from 'styled-components/native';
 import AppLoading from 'expo-app-loading';
+import { AdMobBanner } from 'expo-ads-admob';
 import {
   Nunito_300Light,
   Nunito_400Regular,
@@ -24,6 +25,17 @@ const App = () => {
     Nunito_700Bold,
   });
 
+  // // Exemplo de uso de interstitial id
+  // useEffect(() => {
+  //   const showInterstitialAd = async () => {
+  //     await AdMobInterstitial.setAdUnitID('ca-app-pub-3940256099942544/1033173712'); // Test ID, Replace with your-admob-unit-id
+  //     await AdMobInterstitial.requestAdAsync({ servePersonalizedAds: true });
+  //     await AdMobInterstitial.showAdAsync();
+  //   };
+
+  //   showInterstitialAd();
+  // }, []);
+
   if (!fontsLoaded) {
     return <AppLoading />;
   }
@@ -40,6 +52,14 @@ const App = () => {
 
           <Routes />
         </AppProvider>
+
+        <AdMobBanner
+          style={{ position: 'absolute', bottom: 0 }}
+          bannerSize="fullBanner"
+          adUnitID={process.env.ADMOB_BANNER_ID_TEST}
+          servePersonalizedAds
+          onDidFailToReceiveAdWithError={(error) => console.log(JSON.stringify(error))}
+        />
       </ThemeProvider>
     </SafeAreaProvider>
   );
